0be77750416d4d68e0704304b556f0965c124dc9
akuzhi2 <31894299+akuzhi2@users.noreply.github.com>
2017-12-11 16:47:38 -0600

    Update QuizList.java

--- src/QuizList.java
+++ src/QuizList.java
@@ -35,9 +35,9 @@ public class QuizList {
 		String[] arr5 = {"Arrogance", "Ambition", "Stubborness"};
 		historical.put("Which quality do you believe hinders you most?" , arr5);
 		String[] arr6 = {"Something new", "Progress on already discovered topic"};
-		historical.put("<br>Would you rather discover something new <br>or make major progress on a disocvered topic?", arr6);
+		historical.put("<br>Would you rather discover something new <br>or make major progress on a discovered topic?", arr6);
 		String[] arr7 = {"Leader", "Follower"};
-		historical.put("<br>Do uncondtionally follow authority,<br> or do you believe that there are exceptions?", arr7);
+		historical.put("<br>Do unconditionally follow authority,<br> or do you believe that there are exceptions?", arr7);
 		String[] shark1 = {"Relaxing on a beach in Hawaii", "Skiing in Colorado","Strolling through cornfields in Illinois", "Sightseeing in London"};
 		shark.put("What vacation activity sounds most appealing?", shark1);
 		String[] shark2 = {"White", "Grey", "Blue", "Brown", "Green"};
@@ -64,7 +64,7 @@ public class QuizList {
 		String[] harry3 = {"Although loyal, you turn them in","Help them, but you plan to keep a token of the evidence as blackmail",
 				"You ask them what they did, and depending on what they did, you either help them or you turn them in",
 				"Surprised, you help your friend, but guilt eats at you"};
-		harry.put("        Your friend tells you to come over. <br>        You walk into your friend’s house, and you see them crying. <br>They said that did something horrible, <br>        and they need your help to cover up their mess. <br>        What do you do?",harry3);
+		harry.put("        Your friend tells you to come over. <br>        You walk into your friendâ€™s house, and you see them crying. <br>They said that did something horrible, <br>        and they need your help to cover up their mess. <br>        What do you do?",harry3);
 		String[] harry4 = {"Anything but Slytherin", "The house I deserve", "(Doesn't Respond)", "I'm not sure"};
 		harry.put("What house do you want to be in?", harry4);
 		String[] harry5 = {"Dog", "Hawk", "Seal", "Crow"};
@@ -74,7 +74,7 @@ public class QuizList {
 				"You calmly confront them." };
 		harry.put("Someone close to you has wronged you. What do you do?", harry6);
 		String[] harry7 = {"Punch them back", "Taze them.", "Step back surprised and ask them why they punched you.", "Knock them out and walk away calmly"};
-		harry.put("<br>         You’re minding your own business, <br>          and suddenly, someone approaches you and, <br>          out of the blue, punches you. <br>         You ___", harry7);
+		harry.put("<br>         Youâ€™re minding your own business, <br>          and suddenly, someone approaches you and, <br>          out of the blue, punches you. <br>         You ___", harry7);
 		String[] harry8 = {"Yes!", "Uh, no..."};
 		harry.put("Do you think ron was a good friend?", harry8);
 		if(quizType == 0) {
